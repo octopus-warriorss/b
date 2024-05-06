7c13c62c336ccd379f5b1f744914af47126cf524
octopus-warriors <107366094+octopus-warriorss@users.noreply.github.com>
2024-05-06 16:13:58 -0300

    Update api.js

--- api.js
+++ api.js
@@ -4,12 +4,18 @@ const fs = require('fs').promises;
 const app = express();
 const PORT = 3000;
 
-const externalGifUrl = 'https://media.discordapp.net/attachments/1213611522623021056/1229000936366800916/D205B05F-C2D9-41CF-B1F1-FFC522741BFF.gif?ex=6639f4d0&is=6638a350&hm=4a65e8c5e9c9723a5cc80cb181ffed37044cdeb05afbaa2f866ff5d11264d3c5&='; // Substitua 'URL_DO_SEU_GIF_AQUI' pelo URL do GIF externo desejado
+app.get('/api/gif', async (req, res) => {
+    try {
+        const filePath = `${__dirname}/gif.gif`; // Obtém o caminho completo do arquivo gif.gif
 
-app.get('/api/gif', (req, res) => {
-    // Redirecione para o URL do GIF externo
-    res.redirect(externalGifUrl);
-});
+        const gifContent = await fs.readFile(filePath);
+
+        res.set('Content-Type', 'image/gif');
+        res.send(gifContent);
+    } catch (error) {
+        console.error('Erro ao obter GIF:', error);
+        res.status(500).send('Erro ao obter GIF');
+    }
 
 
 // Rota para obter o conteúdo bruto do arquivo pagina.txt
