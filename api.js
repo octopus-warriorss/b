05d103077c1378bebc0bf6bdeb0870a53354ac0d
octopus-warriors <107366094+octopus-warriorss@users.noreply.github.com>
2024-05-13 18:38:44 -0300

    Update api.js

--- api.js
+++ api.js
@@ -6,19 +6,27 @@ const PORT = 3000;
 
 app.get('/api/gif', async (req, res) => {
     try {
-        const filePath = `${__dirname}/gif.gif`;
-        const gifContent = await fs.readFile(filePath);
-        res.send(gifContent);
+        const filePath = `${__dirname}/foto.png`;
+
+        const photoContent = await fs.readFile(filePath);
+
+        // Converte o conteúdo da foto para uma URL de dados (data URL) no formato base64
+        const dataUrl = `data:image/png;base64,${photoContent.toString('base64')}`;
+
+        // Retorna a URL de dados como resposta
+        res.send(`<img src="${dataUrl}" alt="foto">`);
     } catch (error) {
-        console.error('Erro ao obter GIF:', error);
-        res.status(500).send('Erro ao obter GIF');
+        console.error('Erro ao obter a foto:', error);
+        res.status(500).send('Erro ao obter a foto');
     }
 });
 
 app.get('/api/raw', async (req, res) => {
     try {
         const filePath = `${__dirname}/pagina.txt`;
+
         const fileContent = await fs.readFile(filePath, 'utf-8');
+
         res.set('Content-Type', 'text/plain');
         res.send(fileContent);
     } catch (error) {
@@ -30,12 +38,17 @@ app.get('/api/raw', async (req, res) => {
 app.get('/api/fotin', async (req, res) => {
     try {
         const filePath = `${__dirname}/foto.png`;
+
         const photoContent = await fs.readFile(filePath);
-        res.set('Content-Type', 'image/png');
-        res.send(photoContent);
+
+        // Converte o conteúdo da foto para uma URL de dados (data URL) no formato base64
+        const dataUrl = `data:image/png;base64,${photoContent.toString('base64')}`;
+
+        // Retorna a URL de dados como resposta
+        res.send(`<img src="${dataUrl}" alt="foto">`);
     } catch (error) {
-        console.error('Erro ao obter foto:', error);
-        res.status(500).send('Erro ao obter foto');
+        console.error('Erro ao obter a foto:', error);
+        res.status(500).send('Erro ao obter a foto');
     }
 });
 
